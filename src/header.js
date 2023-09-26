1ce0be699a6a307a55919a86571e41b398bd7778
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-09-26 23:32:12 +0100

    getting the header fuction to be create so it can be imported to another module later on

---
+++ src/header.js
@@ -0,0 +1,29 @@
+const createHeader = () => {
+  const headerTag = document.createElement("header");
+  const headerContainer = document.createElement("div");
+  headerContainer.classList.add("restaurant-title");
+
+  const boatSpan = document.createElement("span");
+  boatSpan.classList.add("material-symbols-outlined");
+  boatSpan.textContent = "sailing";
+
+  const title = document.createElement("h1");
+  title.textContent = boatSpan.value + " The Baratie";
+
+  const navBar = document.createElement("nav");
+  navBar.id = "menu-bar";
+  const ulList = document.createElement("ul");
+  const liItem = ["Home", "Menu", "About"];
+
+  liItem.forEach((e) => {
+    const li = document.createElement("li");
+    li.id = e.toLowerCase;
+    li.textContent = e;
+
+    ulList.appendChild(li);
+  });
+
+  navBar.append(ulList);
+
+  headerTag.append(headerContainer);
+};
