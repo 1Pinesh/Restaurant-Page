4ae83c47759ed79fd9dc229fb83d76b959ad888f
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-10-04 17:13:06 +0100

    webpack is configured and main js has now been used showing configuration successful

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
+import _ from "lodash";
+
 import createHeader from "../src/header.js";
 import homeContent from "../src/homeContent.js";
 import createMenu from "../src/menu.js";
 
-createHeader();
-createMenu();
+const IndexController = () => {
+  createHeader();
 
-const state = {
-  HOMESTATE: 0,
-  MENUSTATE: 1,
-  ABOUTSTATE: 2,
+  // Getting Nav ids
+  const homeNav = document.getElementById("home");
+  const menuNav = document.getElementById("menu");
+  const aboutNav = document.getElementById("about");
+
+  const state = {
+    HOMESTATE: 0,
+    MENUSTATE: 1,
+    ABOUTSTATE: 2,
+  };
+  let currentState = state.HOMESTATE;
+
+  homeNav.addEventListener("click", () => {
+    currentState = state.HOMESTATE;
+    controlState();
+  });
+  menuNav.addEventListener("click", () => {
+    currentState = state.MENUSTATE;
+    controlState();
+  });
+
+  aboutNav.addEventListener;
+
+  const controlState = () => {
+    switch (currentState) {
+      case state.HOMESTATE:
+        homeContent();
+        break;
+      case state.MENUSTATE:
+        createMenu();
+        break;
+      case state.ABOUTSTATE:
+        break;
+    }
+  };
+
+  controlState();
 };
+
+IndexController();
+
+function component() {
+  const elements = document.createElement("div");
+}
