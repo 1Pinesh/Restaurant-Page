9a9a33fa2dff5702026e87e4daceb3b9cb1c3b13
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-10-04 20:02:46 +0100

    The template of contact us has been made now trandfering into js

--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,7 @@ const createContact = () => {
   const contentBox = document.getElementById("content");
   // clearing the content inside the content box;
   contentBox.innerHTML = "";
+
   // pirate-scroll con
   const pirate_ScrollDiv = document.createElement("div");
   pirate_ScrollDiv.classList.add("pirate-scroll");
@@ -9,6 +10,8 @@ const createContact = () => {
   const pirateContent = document.createElement("div");
   pirateContent.classList.add("pirate-content");
   pirateContent.classList.add("home");
+
+  // Creating Contact  Con
 
   // Appeding
   contentBox.appendChild(pirate_ScrollDiv);
